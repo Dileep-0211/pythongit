da3b5b9077f96f804e85199ecf4139820ad2aac6
malepu dileep <malepudileep.111@gmail.com>
2024-07-19 12:48:32 +0530

    addng new ex13
    Signed-off-by: malepu dileep <malepudileep.111@gmail.com>

---
+++ ex13.py
@@ -0,0 +1,10 @@
+#importing the system module 
+#while executing should give python3.6 (first 2nd 3rd)
+from sys import argv
+script, first, second, third = argv
+
+
+print("the script is called:",script)
+print("your first variable is:",first)
+print("your second variable is:",second)
+print("your thirs variable is:",third)
