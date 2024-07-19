daec269e88734e3788d473622c1d9234bb4cd6ed
malepu dileep <malepudileep.111@gmail.com>
2024-07-19 12:44:59 +0530

    addng ex12,13
    Signed-off-by: malepu dileep <malepudileep.111@gmail.com>

---
+++ ex12.py
@@ -0,0 +1,11 @@
+# inthios also inputs
+
+
+name=input("what is your name")
+age=input("what is your age")
+height=input("what is your height")
+
+print(f"so you're name is {name},{age} years old and {height} tall")
+
+
+
